e63008d086127441c6fef6dfaaf2619243377ec1
di-was <diwasdahal109@gmail.com>
2022-09-23 20:07:11 +0545

    Main component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import 'bootstrap/dist/css/bootstrap.min.css'; // BootStrap
+// Custom Css
+import './css/main.css'; 
+
+
+// Components
+import WritingBox from "./components/WritingBox/WritingBox";
+
+
+function App() {
+  return (
+    <WritingBox paragraph="we are gods, so we shall create."/>
+  );  
+}
+
+export default App;

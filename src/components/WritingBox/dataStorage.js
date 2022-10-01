3448d6a634d550f3d3c3282597322f3b39181bab
di-was <diwasdahal109@gmail.com>
2022-10-01 20:52:41 +0545

    bug

--- src/components/WritingBox/dataStorage.js
+++ src/components/WritingBox/dataStorage.js
@@ -1,5 +1,38 @@
 const  dataStorage   = string => {
-   
+  let splitWord = string.toString().replace('’', "'").split(/(\s+)/)
+  let dataStorageData = {}
+  for (var i=0; i < splitWord.length; i++) {
+    
+    dataStorageData = {
+      [i] : {
+        word: splitWord[i],
+        letters: splitWordIntoLetters(splitWord[i]),
+        status: 'notcompleted',
+      },
+      ...dataStorageData
+      }
+  }
+  return dataStorageData
+
+}
+
+const splitWordIntoLetters = word => {
+let wordList = word.split('')
+let wordData = {} 
+for (var i=0; i < wordList.length; i++) {
+
+  wordData = {
+    [i]: {
+      letter:  wordList[i],
+      status: 'notcompleted',
+    }
+    ,
+    ...wordData
+  }
+
+}
+
+return wordData
 }
 
 export default dataStorage
